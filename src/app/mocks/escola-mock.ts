15511ed8f18a9eac2e159d24e1aa128284b2ce7e
Patrícia Martins Rocca Crestani <patricia.crestani@viceri.com.br>
2025-02-25 17:55:31 -0300

    feat: implementa lógica do menu master na listagem de pessoas

--- src/app/mocks/escola-mock.ts
+++ src/app/mocks/escola-mock.ts
@@ -7,37 +7,41 @@ export const EscolasMock = [
             { status: 200 }
         );
     }),
-
     http.get('https://example.com/escolas', () => {
         return HttpResponse.json(
             [
                { 
                     "id": 1,
                     "cnpj": "14.166.648/0001-47",
-                    "nome": "Escola A"
+                    "nome": "Escola A",
+                    "cadastroFinalizado": true
                 },
                 {
                     "id": 2,
                     "cnpj": "35.783.453/0001-16",
-                    "nome": "Escola B"
+                    "nome": "Escola B",
+                    "cadastroFinalizado": true
                 },
                 {
                     "id": 3,
                     "cnpj": "63.344.553/0001-81",
-                    "nome": "Escola C"
+                    "nome": "Escola C",
+                    "cadastroFinalizado": true
                 },
                 {
                     "id": 4,
                     "cnpj": "68.008.877/0001-62",
-                    "nome": "Escola D"
+                    "nome": "Escola D",
+                    "cadastroFinalizado": true
                 },
                 {
                     "id": 5,
                     "cnpj": "02.170.854/0001-41",
-                    "nome": "Escola E"
+                    "nome": "Escola E",
+                    "cadastroFinalizado": true
                 }
             ],
             { status: 200 }
         );
-    }),
+    })
 ];
